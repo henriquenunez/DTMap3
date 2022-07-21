4b507be301049dec42093ba88d57d94b087a942a
Henrique Hiram <henrique.nunez@yahoo.com>
2022-07-21 23:21:20 +0200

    feat: renderText definitions

---
+++ src/renderText.cpp
@@ -0,0 +1,27 @@
+// import freetype and stuff.
+
+class Text
+{
+public:
+    // Internal Buffers
+    Text()
+    {
+        // Initialize font idk
+        
+    }
+
+    // Adds right aligned text ending at this position (upper corner)
+    void rightAligned(float x, float y, std::string text)
+    {
+        // Dont have to initialize all the opengl stuff all the time, right?
+
+    }
+
+    void render()
+    {
+        // The usual procedure
+    }
+
+}
+
+
